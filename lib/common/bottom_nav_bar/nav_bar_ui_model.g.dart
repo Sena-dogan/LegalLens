b071515a91cc36571843c7b9bc945213413e7a23
Zeynep Sena Doğan <93337596+Sena-dogan@users.noreply.github.com>
2024-06-29 17:23:15 +0300

    Initial commit

---
+++ lib/common/bottom_nav_bar/nav_bar_ui_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'nav_bar_ui_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$BottomNavBarUiModelImpl _$$BottomNavBarUiModelImplFromJson(
+        Map<String, dynamic> json) =>
+    _$BottomNavBarUiModelImpl(
+      navIndex: json['navIndex'] as int? ?? 0,
+    );
+
+Map<String, dynamic> _$$BottomNavBarUiModelImplToJson(
+        _$BottomNavBarUiModelImpl instance) =>
+    <String, dynamic>{
+      'navIndex': instance.navIndex,
+    };
---
+++ lib/constants/endpoints.dart
@@ -0,0 +1,8 @@
+class Endpoints {
+  Endpoints._();
+  static const String baseUrl = '';
+  static const int receiveTimeout = 15000;
+  static const int connectionTimeout = 30000;
+
+  // booking endpoints
+}
